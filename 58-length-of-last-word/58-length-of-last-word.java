5a50021fa6877553e90f07b59406038444863d9f
akshaywakhare <65349910+akshaywakhare@users.noreply.github.com>
2022-03-24 14:32:52 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (83.96%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -2,7 +2,7 @@ class Solution {
     public int lengthOfLastWord(String s) {
         s.trim();
         // StringTokenizer  st=new StringTokenizer(s," ");
-         String[] result = s.split("\\s");
+         String[] result = s.split(" ");
         // String ss="";
         // while(st.hasMoreTokens()){ss=st.nextToken();}
     return result[result.length-1].length();}
